a0d13fb27ae2b1cace291e5c1868a25f5708a35b
Dishendra <dishendramishra@gmail.com>
2024-12-16 16:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def download_file(name):
 if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port='80')
